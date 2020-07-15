3edd73968dee43f7614611bf04f6168e2bf2cbd1
MaslyukSveta <masluksveta2002@gmail.com>
2020-07-15 16:47:49 +0300

    Exampl

---
+++ HW_1/src/Task2.java
@@ -0,0 +1,9 @@
+public class Task2 {
+
+    public static void main (String args[]) {
+
+      for (int i = 5; i >= 0; i--) {
+        System.out.print(args[i] + " ");
+      }
+    }
+}
---
+++ HW_1/src/Task3.java
@@ -0,0 +1,17 @@
+public class Task3 {
+
+  public static void main (String [] args) {
+
+		int [] numbers = { 1, 23, 456, 7890, 12345, 678901, 2345678, 90123456, 789012345, 1234567890};
+
+		int n = 10;
+
+		for (int i = 0; i < n; i++) {
+			System.out.println(numbers [i]);
+		}
+
+    for (int i = 0; i < n; i++) {
+			System.out.print(numbers [i] + " ");
+		}
+	}
+}
